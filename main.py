a4a265e0bee2c1e87e3b45e1bf5c33eb8fd720c4
beratcano <beratcanozkan@ogr.iu.edu.tr>
2024-09-19 13:51:14 +0200

    Start the project with creating the template based on instructions.

---
+++ main.py
@@ -0,0 +1,41 @@
+#import libraries
+
+class Main:
+    def __init__(self):
+        print()
+        # constructor, i dont know if we need it yet!
+
+    def play_game():
+        print()
+        # is the main function, which performs the following tasks:
+        # 1) Calls create_board() to create a 3×3 board and initializes with 0.
+        # 2) For each player (1 or 2), calls the random_place() function to randomly choose a location
+        #    on board and mark that location with the player number, alternatively.
+        # 3) Print the board after each move.
+        # 4) Evaluate the board after each move to check whether a row or column or diagonal has
+        #    the same player number. If so, displays the winner’s name. If after 9 moves, there is no
+        #    winner then displays -1
+    
+    def possibilities():
+        print()
+        # chosing random place to put (for computers move)
+
+    def rol_win():
+        print()
+        # checking row wise winner
+    
+    def col_win():
+        print()
+        # checking column wise winner
+
+    def diag_win():
+        print()
+        # checking diagonal wise winner
+    
+    def no_win():
+        print()
+        # checking if there is no winner
+    
+    def evaluate():
+        print()
+        # evaluating the board after each move
